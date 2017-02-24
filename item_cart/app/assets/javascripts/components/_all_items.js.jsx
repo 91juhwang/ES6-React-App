e0d3269280681a106192065b978b98cee493e090
91juhwang <91juhwang@gmail.com>
2017-02-24 17:29:46 +0100

    Modify new_item render for new items

--- item_cart/app/assets/javascripts/components/_all_items.js.jsx
+++ item_cart/app/assets/javascripts/components/_all_items.js.jsx
@@ -9,7 +9,7 @@ var AllItems = React.createClass({
 
   render() {
     // .map method is similar to .each in ruby 
-    var items = this.state.items.map((item) => { 
+    var items = this.props.items.map((item) => { 
       return (
         <div key={item.id}>
           <h3> { item.name } </h3>
