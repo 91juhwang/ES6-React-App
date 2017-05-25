645cc3476bcbec2283532e1a6dab87b2abff844c
James Hwang <91juhwang@gmail.com>
2017-05-25 11:57:09 -0400

    Add basic methods for working tic-tac-toe game

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'My Project!!',
+      title: 'React Projects!!',
       minify: {
         collapseWhitespace: true
       },
